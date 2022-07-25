08cd8b3c8656ba2789833ef23cac630f6f5a698a
Ahmed <syedahmedmaliksam@gmail.com>
2022-07-25 03:17:35 -0700

    Omerrrrr Camera Edits

--- Emo Go - Copy/Assets/Scripts/PlatformControllerAlt.cs
+++ Emo Go - Copy/Assets/Scripts/PlatformControllerAlt.cs
@@ -7,15 +7,30 @@ public class PlatformControllerAlt : MonoBehaviour
 
     private float _xRotation;
     private float _zRotation;
-    private float _movementMultiplier = 10f;
+    private float _movementMultiplier = 7f;
 
     private Touch _touch;
+
+    Camera _cam;
+
+
+    Vector3 _camNormalPos;
+    Vector3 _camUpPos;
+    Quaternion _camNormalRotation;
+    Quaternion _camUpRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         _xRotation = 0;
         _zRotation = 0;
+        _cam = Camera.main;
+
+        _camNormalPos = _cam.transform.position;
+        _camUpPos = new Vector3(_cam.transform.position.x, 20f, _cam.transform.position.z + 8);
+
+        _camNormalRotation = _cam.transform.rotation;
+        _camUpRotation = Quaternion.Euler(76, 0, 0);
     }
 
     // Update is called once per frame
@@ -30,7 +45,6 @@ public class PlatformControllerAlt : MonoBehaviour
             {
                 _xRotation = Mathf.Lerp(_xRotation, _xRotation + _touch.deltaPosition.y, Time.deltaTime * _movementMultiplier);
                 _zRotation = Mathf.Lerp(_zRotation, _zRotation - _touch.deltaPosition.x, Time.deltaTime * _movementMultiplier);
-
             }
 
         }
@@ -42,6 +56,17 @@ public class PlatformControllerAlt : MonoBehaviour
         _zRotation = Mathf.Clamp(_zRotation, -25, 25);
 
         gameObject.transform.rotation = Quaternion.Euler(_xRotation, 0, _zRotation);
+
+        if(_xRotation >= 27)
+        {
+            _cam.transform.position = Vector3.Lerp(_cam.transform.position, _camUpPos, 5 * Time.deltaTime);
+            _cam.transform.rotation = Quaternion.Slerp(_cam.transform.rotation, _camUpRotation, 5 * Time.deltaTime);
+        }
+        else
+        {
+            _cam.transform.position = Vector3.Lerp(_cam.transform.position, _camNormalPos, 5 * Time.deltaTime);
+            _cam.transform.rotation = Quaternion.Slerp(_cam.transform.rotation, _camNormalRotation, 5 * Time.deltaTime);
+        }
 
     }
 }
